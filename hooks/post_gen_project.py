1eb11efd88c5df56bbf58d2cd599713ad2ab8ba8
Etuloser <1494136313@qq.com>
2023-06-12 11:27:12 +0800

    feature: add use—_vue3 options

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,15 @@
 # 模版生成后执行动作
+import shutil
+
+
+def remove_websites():
+    shutil.rmtree("websites")
+
+
+def main():
+    if "{{ cookiecutter.use_vue3 }}" == "n":
+        remove_websites()
+
+
+if __name__ == "__main__":
+    main()
