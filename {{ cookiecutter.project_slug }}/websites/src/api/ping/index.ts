5d066305b18814bc13b1d590339cd094cd285ab1
Etuloser <1494136313@qq.com>
2023-06-27 14:14:17 +0800

    feature: add drf-spectacular

--- {{ cookiecutter.project_slug }}/websites/src/api/ping/index.ts
+++ {{ cookiecutter.project_slug }}/websites/src/api/ping/index.ts
@@ -2,7 +2,7 @@ import request from "@/utils/request.ts"
 
 export default function ping() {
   return request({
-    url: "/api/v1/drfadmin/ping/",
+    url: "/api/v1/{{ cookiecutter.project_slug }}/ping/",
     method: "get"
   })
 }
